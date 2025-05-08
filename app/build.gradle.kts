18610fed47e2c6175f47e07389b9d60c32db7268
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-05-08 12:17:08 +0100

    dokka markdown

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.23"
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
+    id("org.jetbrains.dokka") version "2.0.0"
 }
 
 group = "com.example"
